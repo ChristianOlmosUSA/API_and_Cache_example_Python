fd3e39d859c4850a645c10b8b5126f0b0ef4d876
Chris <christianOlmos@protonmail.com>
2020-05-05 00:49:40 +0000

    API fetch and example local caching from Urban Dictionary

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
-import requests, os
+import requests, os, json
+
+#### TO RUN:  pipenv run python src/app.py
 
 # Retrieve your API credentials from the .env file
 if os.getenv("API_KEY") is None or os.getenv("API_KEY") == "":
@@ -6,7 +8,48 @@ if os.getenv("API_KEY") is None or os.getenv("API_KEY") == "":
     exit(1)
 
 # Get credentials from the .env file
-API_HOST = os.getenv("API_HOST")
+API_HOST = os.getenv("API_HOST") 
 API_KEY = os.getenv("API_KEY")
 
 # continue with your application here
+
+def sync_word(_user_response, _permission):                 # opens and reads the file, reading or writing depending on permission passed
+    text_file = open("./src/" + _user_response +".txt", _permission)       #WAS: with open(user_response +".txt", "w") as text_file:      
+    if _permission == "w+":         
+        text_file.write(body["list"][0]["definition"])      
+    else:
+        return text_file.read()
+    text_file.close()    
+
+user_response = input("What term do you want to look for?  ")
+
+if os.path.isfile("./src/" + user_response + ".txt"):           # removed if user_path == , as the isfile will give a true/false anyway
+    print("fetching from cache")
+    print(sync_word(user_response, "r"))
+else: 
+
+
+    url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term="+user_response
+
+    headers = {
+        'x-rapidapi-host': API_HOST,
+        'x-rapidapi-key': API_KEY
+        }
+
+    response = requests.request("GET", url, headers=headers)
+
+    body = response.json()
+
+    definitions_array = body["list"]
+
+    single_definition_from_dict = definitions_array[0]
+
+
+    print(single_definition_from_dict)
+    #print(response.text)
+
+    print(body["list"][0]["definition"])
+    sync_word(user_response,"w+")
+
+                                                 # always be closing files you open!
+    
